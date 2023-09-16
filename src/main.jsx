03e1f5c9cc5056e4be0c3e85abf4124124ff8286
Haris Bin Waseem <hariswaseem430@gmail.com>
2023-09-16 22:58:52 +0500

    RTK Query Implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react/";
-// import { Store } from "./Store.jsx";
-import { PostAPI } from "./Redux/API.jsx";
+import { store } from "./store.jsx";
+import { postsApi } from "./Redux/API.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={Store}> */}
-    <ApiProvider api={PostAPI}>
-      <App />
-    </ApiProvider>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <ApiProvider api={postsApi}>
+        <App />
+      </ApiProvider>
+    </Provider>
   </React.StrictMode>
 );
